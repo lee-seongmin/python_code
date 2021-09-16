84e5c01a24de036aa9e9735d6c777093c4e202f7
lee-seongmin <86665848+lee-seongmin@users.noreply.github.com>
2021-09-16 16:41:48 +0900

    Update 09_16_spyder_2.py

--- 09_16_python_class/09_16_spyder_2.py
+++ 09_16_python_class/09_16_spyder_2.py
@@ -14,3 +14,29 @@ for i in range(0,100):
 print("1부터 100사이의 모든 값은 {0}".format(hap))
 print("1부터 100사이의 3의배수의 개수는 {0}".format(ans))
 #%%
+import random
+com_ans=random.random(1,101)
+
+print("숫자게임에 오신것을 환영합니다.")
+
+cnt=0
+btn='Y' or 'y'
+
+while(btn=='y' or 'Y'):
+    if(btn=='y' or btn=='Y'):
+        ans=int(input("숫자를 맞춰 보세요 : "))
+    
+        if(ans>com_ans):
+            print("너무 큼!")
+        elif(ans<com_ans):
+            print("작음!")
+        else:
+            print("!맞췄습니다!")
+        cnt+=1
+        print("{0}회차 시도".format(cnt))
+    else:
+        break
+    
+    btn=(str(input("계속하시겠습니까?")))
+    
+#%%
