f2ca146e4aaca0102e741324d20598535e6e7daa
lee-seongmin <86665848+lee-seongmin@users.noreply.github.com>
2021-10-15 17:35:23 +0900

    Update homework2.py

--- 21812411 10_14/homework2.py
+++ 21812411 10_14/homework2.py
@@ -3,22 +3,22 @@ from math import *
 #레이블 1개에 버튼이 4개 버튼 구조는 label row=0, button row=1에 column2개 row2에 column2개
 
 def click(key):
-    if key =='10':
-        try:
-            result = eval(entry.get())
-            entry.delete(0, END)
-            entry.insert(END, str(result))
-        except:
-            entry.insert(END, "오류!")
+    if key =='=':
+        result = eval(e.get())
+        e.delete(0, END)
+        e.insert(END, str(result))
+    else:
+        e.insert(END,key)
 window = Tk()
 
 e = Entry(window)
-e.grid(row=0, columnspan=2)
+e.grid(row=0, columnspan=2, sticky='ew',ipadx=100,ipady=25)
 
-b1 = Button(window,text = "10").grid(row=1,column=0)
-b2 = Button(window,text = "+").grid(row=1,column=1)
-b3 = Button(window, text = "100").grid(row=2,column=0)
-b4 = Button(window,text = "=").grid(row=2,column=1)
+button = ["10", "+", "100", "="]
+b1 = Button(window,text = "10", command = lambda : click('10')).grid(row=1,column=0, sticky='ew',ipady=25)
+b2 = Button(window,text = "+", command = lambda : click('+')).grid(row=1,column=1, sticky='ew',ipady=25)
+b3 = Button(window, text = "100", command = lambda : click('100')).grid(row=2,column=0, sticky='ew',ipady=25)
+b4 = Button(window,text = "=", command = lambda : click('=')).grid(row=2,column=1, sticky='ew',ipady=25)
 
 e.bind("=",click)
-window.mainloop()
+window.mainloop() 
